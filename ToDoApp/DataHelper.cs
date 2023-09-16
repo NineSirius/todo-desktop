b8a5c9857dc1f04a0de02772ac7c4f3cfa0e861a
NineSirius <ruslanbulah3@gmail.com>
2023-09-16 12:19:14 +0600

    Remove json.net package and isntall Newtonsoft.Json

---
+++ ToDoApp/DataHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using Newtonsoft.Json;
+using ToDoApp.ViewModels;
+
+namespace ToDoApp
+{
+    public class DataHelper
+    {
+        // Метод для сохранения задач в JSON файл
+        public static void SaveTasks(List<TaskViewModel> tasks, string filePath)
+        {
+            string json = JsonConvert.SerializeObject(tasks, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+        }
+
+        // Метод для загрузки задач из JSON файла
+        public static List<TaskViewModel> LoadTasks(string filePath)
+        {
+            List<TaskViewModel> tasks = new List<TaskViewModel>();
+
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                tasks = JsonConvert.DeserializeObject<List<TaskViewModel>>(json);
+            }
+
+            return tasks;
+        }
+    }
+}
